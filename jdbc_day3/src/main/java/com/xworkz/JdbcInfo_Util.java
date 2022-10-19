e3e2c288ed27cf1f117f63bc752a248fe7cacb75
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-10-19 20:30:16 +0530

    committing jpa files

--- jdbc_day3/src/main/java/com/xworkz/JdbcInfo_Util.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz;
-
-public class JdbcInfo_Util {   //UTIL=COMMON CLASSES
-
-	public static final String URL="jdbc:mysql://localhost:3306";
-	public static final String USERNAME="root";
-	public static final String PASSWORD="Xworkzodc@123";
-}
